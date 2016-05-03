1d2ebbf27aaaf1468fa1536afd471966567da8f5
Wouter de Vos <wouter@codaisseur.com>
2016-05-03 10:40:34 +0200

    Use live api

--- src/models/BaseModel.js
+++ src/models/BaseModel.js
@@ -2,7 +2,7 @@ import SocketClient from 'socket.io-client';
 import Feathers from 'feathers-client';
 import Utils from '../lib/Utils';
 
-const API_HOST = "http://localhost:3030";
+const API_HOST = "https://protected-anchorage-55121.herokuapp.com";
 
 class BaseModel {
   defaults() { return {}; }
